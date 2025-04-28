ce00110779a542df01b521339c2d38fb3383b456
lodiped <pedroelodi@gmail.com>
2025-04-28 08:58:43 -0300

    logo placeholder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 	import { check, userArray } from '$lib/state.svelte';
 	import Leaderboard from '$lib/Components/Leaderboard.svelte';
+	import moveverso from '$lib/assets/moveverso.png';
 	let loading = $state(true);
 	let sortedUsers = $state([] as any[]);
 	let error = $state(false);
@@ -19,7 +20,10 @@
 	});
 </script>
 
-<div class="flex w-[1000px] flex-col gap-2">
+<div class="mt-10 flex w-[1000px] flex-col items-center gap-15">
+	<div class="w-1/2">
+		<img src={moveverso} class="drop-shadow-accent/50 drop-shadow-[0_0_20px]" alt="" />
+	</div>
 	{#if loading}
 		<div class="w-full text-center">loading</div>
 	{:else}
